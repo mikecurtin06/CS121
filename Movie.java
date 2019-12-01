445ddb3280a08da8571dc92f73bbda66f0e6ccc6
mikecurtin06 <56272470+mikecurtin06@users.noreply.github.com>
2019-12-01 12:45:24 -0500

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,36 @@
+/**
+ * This class encapsulates the data required to represent a movie in a collection
+ * of MediaItems. It derives from MediaItem.
+ * In addition to its superclass attributes, the attributes of a movie are: 
+ *       int: playing time (minutes), String; lead actor, and String: year of release.
+ **/
+ public class Movie extends MediaItem {
+  /* TODO: instance variable declarations */
+
+  
+   /* TODO: Constructor*/ 
+  public Movie(String title, String author, String genre, 
+                   int playTime, String leadActor, String releaseYear){
+
+  }
+  
+  //TODO: write these three get methods: getPlayTime, getLeadActor, getReleaseYear
+
+
+
+
+
+
+
+  
+  /* TODO: Override the superclass toString method. Use a call to the superclass toString method
+   * to get the superclass attributes.
+   * The return should have the format:
+   *   Movie: [title], [author], [genre], [playTime], [leadActor], [releaseYear]
+   * For example:
+   *   "Movie: Black Panther, Coogler, fantasy, 134, Chadwick Boseman, 2018"
+   */
+  public String toString(){
+     return null;
+  }
+}
